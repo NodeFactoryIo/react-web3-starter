cab9db50681a02e5f554aef69420cd6bc239f38f
Bernard <bero4net@gmail.com>
2020-10-16 12:05:14 +0200

    implement root state

---
+++ src/ducks/store.ts
@@ -0,0 +1,24 @@
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
+import reduxSaga from 'redux-saga';
+import rootSaga from './rootSaga';
+
+// state
+const rootReducer = combineReducers({});
+export type RootState = ReturnType<typeof rootReducer>;
+
+// middlewares
+const sagaMiddleware = reduxSaga();
+const middleware = [sagaMiddleware];
+
+// store
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    process.env.NODE_ENV === 'production'
+        ? applyMiddleware(...middleware)
+        : composeEnhancers(applyMiddleware(...middleware)),
+);
+
+export default store;
+
+sagaMiddleware.run(rootSaga);
