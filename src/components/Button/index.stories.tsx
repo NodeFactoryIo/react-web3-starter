2644deeb21d1eba571d6c1771e433b2e9a626f24
Bernard <bero4net@gmail.com>
2020-10-20 12:45:17 +0200

    refactor storybook

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { Button, ButtonProps } from './Button';
+import { Button, ButtonProps } from './index';
 
 export default {
-    title: 'Example/Button',
+    title: 'Core/Button',
     component: Button,
     argTypes: {
         backgroundColor: { control: 'color' },
