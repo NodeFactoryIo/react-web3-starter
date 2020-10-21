d07e0c87ede77907b89bcf4e6a2efffe1a6661ae
Bernard <bero4net@gmail.com>
2020-10-21 14:23:03 +0200

    fix

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,10 +1,4 @@
-export type Await<T> = T extends {
-    // TODO: remove at typescript version 4.x
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    then(onfulfilled?: (value: infer U) => unknown): unknown;
-}
-    ? U
-    : T;
+export type Await<T> = T extends Promise<infer U> ? U : T;
 
 export type DeepPartial<T> = {
     [P in keyof T]?: DeepPartial<T[P]>;
