3e1a77859f33ec59e103b374b86ab2decd12bee9
Bernard <bero4net@gmail.com>
2020-10-21 12:50:00 +0200

    lint

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import { render as rtlRender, RenderOptions, RenderResult } from '@testing-library/react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
