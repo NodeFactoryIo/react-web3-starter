1ab0d2e569f4c159bfb7badaa7f23b1b3c043193
Bernard <bero4net@gmail.com>
2020-10-22 09:42:19 +0200

    implement header

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,12 +4,14 @@ import './App.scss';
 import { HomePage } from './Home';
 import { Provider } from 'react-redux';
 import store from '../ducks/store';
+import Header from '../containers/Header';
 
 const App: FC = () => {
     return (
         <Suspense fallback={null}>
             <BrowserRouter>
                 <Provider store={store}>
+                    <Header />
                     <Switch>
                         <Route exact path='/' component={HomePage} />
                     </Switch>
